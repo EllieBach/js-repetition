5ff30b276510b4bd560efdfd080c11681d1ad88b
EllieBach <80790209+EllieBach@users.noreply.github.com>
2023-12-25 08:30:53 +0100

    git df

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-for (let i = 1; i <= 1000; i++) {
+//loopar
+
+/* for (let i = 1; i <= 1000; i++) {
   console.log("varv" + i);
+}
+
+ */
+
+/* for (let i = 100; i > 0; i--) {
+  console.log(i);
+} */
+
+let fruits = ["apple", "orange", "pear", "kiwi", "pineapple"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
 }
